1840d6d4eee1cfac041f0ced6ba85fb2e8692912
Savostov-Arseny <savostov.arseny@gmail.com>
2019-10-12 03:40:42 +0400

    Add /data & /log folders creation

--- j-build.sh
+++ j-build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 read -p "Set the variable jenkins-user: " juser
 read -sp "Set the variable jenkins-password: " jpass
+#Creates folders that will mounted as volumes
+if [[ ! -d "./master/data" ]]; then
+    mkdir "./master/data"
+fi
+if [[ ! -d "./master/log" ]]; then
+    mkdir "./master/log"
+fi
+if [[ ! -d "./nexus/data" ]]; then
+    mkdir "./nexus/data"
 #read -p "Provide URL where the seed job placed: " SEED_JOBS_URL
 rm -rf ./master/data/* ./master/log/* ./nexus/data/*
 echo $juser > ./master/data/juser && echo $jpass > ./master/data/jpass
